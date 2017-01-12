981d7f5fa68f7b458401b725d1af1891fd3abfba
Austin Heiman <atheimanksu@gmail.com>
2017-01-12 10:24:20 -0600

    metadata updates

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,5 @@ description 'Installs/Configures resources'
 long_description 'Installs/Configures resources'
 version '0.1.0'
 
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/resources/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/resources' if respond_to?(:source_url)
+issues_url 'https://github.com/atheiman/resources-cookbook/issues'
+source_url 'https://github.com/atheiman/resources-cookbook'
